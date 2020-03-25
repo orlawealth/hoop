c84e00c1010b2da34da97697a2e5be7fb560ba77
orlawealth <luv4junior@yahoo.com>
2020-03-25 16:33:10 +0100

    changed phone number in footer

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <div class="bg-contact">
 		<div class="text-center white p-5">
 			<h3>Contact Us</h3>
-			<span>18b, Estaport Avenue, Sholuyi Gbagada	<br> Lagos State <br> +234 806 000 0000 <br>+234 906 000 0000 <br> info@hooptelecoms.com
+			<span>18b, Estaport Avenue, Sholuyi Gbagada	<br> Lagos State <br> +234 806 050 0000 <br>+234 906 000 0000 <br> info@hooptelecoms.com
 			</span>
 			<div class="pb-5">
   			  <a href="https://web.facebook.com/HoopLimited/" target="_blank" class="icon"><i class="fa fa-facebook"></i></a>
